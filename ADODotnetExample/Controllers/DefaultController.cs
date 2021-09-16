4b1fabf9fee8137cdde9deabfb5b818c5819dcec
yawar2019 <yawarali17@gmail.com>
2021-09-16 08:02:12 +0530

    completed

--- ADODotnetExample/Controllers/DefaultController.cs
+++ ADODotnetExample/Controllers/DefaultController.cs
@@ -32,5 +32,44 @@ namespace ADODotnetExample.Controllers
             
               return View();  
         }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.getEmployeeDetailsById(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            int i = db.UpdateEmployee(emp);
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            EmployeeModel emp = db.getEmployeeDetailsById(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            int i = db.DeleteEmployee(id);
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
--- ADODotnetExample/Models/EmployeeContext.cs
+++ ADODotnetExample/Models/EmployeeContext.cs
@@ -36,8 +36,60 @@ namespace ADODotnetExample.Models
             SqlCommand cmd = new SqlCommand("sp_CreateEmployee", con);//storeprocName
             cmd.CommandType = CommandType.StoredProcedure;
             con.Open();
+
             cmd.Parameters.AddWithValue("@EmpName", emp.EmpName);//PArameter name
             cmd.Parameters.AddWithValue("@EmpSalary", emp.EmpSalary);
+            int i = cmd.ExecuteNonQuery();//Execute Query
+            con.Close();
+            return i;
+        }
+
+
+        
+
+            public EmployeeModel getEmployeeDetailsById(int? id)
+        {
+            EmployeeModel emp = new EmployeeModel();
+            SqlCommand cmd = new SqlCommand("usp_getEmployeesById", con);
+            cmd.Parameters.AddWithValue("@EmpId", id);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+            }
+            return emp;
+        }
+        public int UpdateEmployee(EmployeeModel emp)
+        {
+
+            SqlCommand cmd = new SqlCommand("usp_updateUmeshEmployee", con);//storeprocName
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+
+            cmd.Parameters.AddWithValue("@empId", emp.EmpId);//PArameter name
+            cmd.Parameters.AddWithValue("@empname", emp.EmpName);//PArameter name
+            cmd.Parameters.AddWithValue("@empsalary", emp.EmpSalary);
+            int i = cmd.ExecuteNonQuery();//Execute Query
+            con.Close();
+            return i;
+        }
+
+        public int DeleteEmployee(int? id)
+        {
+
+            SqlCommand cmd = new SqlCommand("usp_DeleteEmployeeById", con);//storeprocName
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+
+            cmd.Parameters.AddWithValue("@EmpId", id);//PArameter name
+             
             int i = cmd.ExecuteNonQuery();//Execute Query
             con.Close();
             return i;
@@ -45,5 +97,4 @@ namespace ADODotnetExample.Models
     }
 }
 
-//Create procedure usp_SaveEmployee
-//@EmpName varchar(60),@EmpSalary int
+ 

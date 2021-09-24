d4bebe7de80abde515f32a9c17ed652d6851a860
yawar2019 <yawarali17@gmail.com>
2021-09-24 08:09:06 +0530

    committed

---
+++ CodeFirstApproach/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstApproach.Models
+{
+    public class EmployeeModel
+    {
+        [Key]//assign primary key and identity column
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSalary { get; set; }
+    }
+}

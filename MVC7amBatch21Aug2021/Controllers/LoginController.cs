088a0cdfe25a8e5d7583b69ed11564fd8f3eb298
yawar2019 <yawarali17@gmail.com>
2021-10-30 08:05:06 +0530

    committed

---
+++ MVC7amBatch21Aug2021/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using MVC7amBatch21Aug2021.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC7amBatch21Aug2021.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+        EmployeeEntities db = new EmployeeEntities();
+
+
+        public ActionResult Login()
+        {
+            EmployeeEntities db = new EmployeeEntities();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserDetail det)
+        {
+            UserDetail emp = db.UserDetails.Where(x => x.UserName == det.UserName && 
+                             x.Password == det.Password).SingleOrDefault();
+            if (emp != null)
+            {
+                FormsAuthentication.SetAuthCookie(det.UserName, false);
+                return RedirectToAction("DashBoard");
+            }
+
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+    }
+
+   
+}
